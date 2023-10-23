4efb475882c966093e50dac11ffe9eae1d7fc009
dormando <dormando@rydia.net>
2023-10-22 17:08:57 -0700

    split out the library a bit more
    using some of these in an extstore-specific test set.

---
+++ suite-lib.lua
@@ -0,0 +1,196 @@
+--
+-- WARMERS --
+--
+
+local FLUSH_AFTER <const> = 50000000
+function warm(a)
+    local count = a.limit
+    local size = a.vsize
+    local prefix = a.prefix
+    local written = 0
+    local shuffle = a.shuffle
+    local sleep = a.sleep
+    local flush_after = FLUSH_AFTER
+    if a.flush_after then
+        flush_after = a.flush_after
+    end
+    local c = mcs.client_new({})
+    if c == nil then
+        print("ERROR: warmer failed to connect to host")
+        return
+    end
+    if mcs.client_connect(c) == false then
+        print("ERROR: warmer failed to connect")
+        return
+    end
+
+    local numbers = {}
+        if shuffle then
+        for i=1,count do
+            table.insert(numbers, i)
+        end
+        -- shuffle
+        for i=#numbers, 2, -1 do
+            local j = math.random(i)
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+        end
+    end
+
+    for i=1,count do
+        local num = i
+        if shuffle then
+            num = numbers[i]
+        end
+        local req = mcs.ms(prefix, num, size, "q")
+        mcs.client_write(c, req)
+        written = written + size
+        if written > flush_after then
+            mcs.client_flush(c)
+            written = 0
+            if sleep then
+                mcs.sleep_millis(sleep)
+            end
+        end
+    end
+
+    mcs.client_write(c, "mn\r\n")
+    mcs.client_flush(c)
+    local res = mcs.res_new()
+    -- TODO: bother validating MN? this doesn't fail here.
+    mcs.client_read(c, res)
+end
+
+--
+-- STATS DISPLAY
+--
+
+function _stat_sample(a)
+    local stats = {}
+    local res = mcs.res_new()
+
+    if a.previous_stats == nil then
+        a.previous_stats = {}
+    end
+    local previous_stats = a.previous_stats
+    while true do
+        mcs.read(res)
+        if mcs.res_startswith(res, "END") then
+            break
+        end
+        stats[mcs.res_statname(res)] = mcs.res_stat(res)
+    end
+
+    for _, s in pairs(a["stats"]) do
+        if previous_stats[s] ~= nil then
+            local count = stats[s] - previous_stats[s]
+            print("stat:", s, ": ", count)
+        end
+    end
+    for _, s in pairs(a["track"]) do
+        if stats[s] ~= nil then
+            print("stat:", s, ": ", stats[s])
+        end
+    end
+
+    a.previous_stats = stats
+end
+
+function proxy_stat_sample(a)
+    mcs.write("stats proxy\r\n")
+    mcs.flush()
+    _stat_sample(a)
+end
+
+function stat_sample(a)
+    mcs.write("stats\r\n")
+    mcs.flush()
+    _stat_sample(a)
+end
+
+--
+-- STATISTICS --
+--
+
+-- we create some small canary values for this timer to fetch so tests with
+-- bandwidth limits, large values, etc, aren't as skewed.
+-- this latency sampler isn't meant to be for super detailed performance, but
+-- to help characterize some performance and failures.
+function timer_metaget(a)
+    local prefix = a.prefix .. "canary"
+    local req = mcs.mg_factory(prefix, "v")
+    local res = mcs.res_new()
+
+    return function()
+        local num = math.random(50)
+        mcs.write_factory(req, num)
+        mcs.flush()
+
+        mcs.read(res)
+
+        local status, elapsed = mcs.match(req, res)
+        -- TODO: split hit vs miss timing?
+        local bucket = math.floor(math.log(elapsed, 10) + 1)
+        -- sub-ms are bucketed logarithmically
+        if bucket > 5 then
+            timer_bounds = timer_bounds + 1
+        elseif bucket > 3 then
+            -- TODO: kill granularity but still bucket above 10ms
+            bucket = math.floor(elapsed / 1000)
+            timer_mshist[bucket] = timer_mshist[bucket] + 1
+        else
+            timer_hist[bucket] = timer_hist[bucket] + 1
+        end
+        --print("timer sample:", elapsed, bucket)
+
+        if mcs.res_startswith(res, "EN") then
+            local set = mcs.ms(prefix, num, 10)
+            mcs.write(set)
+            mcs.flush()
+            mcs.read(res)
+            -- TODO: this shouldn't fail, but we can still check the response.
+        end
+    end
+end
+
+-- uses globals to share data with timer funcs.
+function timer_display(a)
+    timer_hist = {}
+    timer_mshist = {}
+    timer_bounds = 0
+
+    for i=1,3 do
+        table.insert(timer_hist, 0)
+    end
+
+    for i=1,100 do
+        table.insert(timer_mshist, 0)
+    end
+
+    -- TODO: use out func to print all at once.
+    return function()
+        print("=== timer ===")
+        print("1us", timer_hist[1])
+        print("10us", timer_hist[2])
+        print("100us", timer_hist[3])
+        for i=1,100 do
+            if timer_mshist[i] > 0 then
+                print(i .. "ms", timer_mshist[i])
+            end
+        end
+        if timer_bounds ~= 0 then
+            print("100ms+:", timer_bounds)
+        end
+        print("=== end ===")
+
+        for i=1,3 do
+            timer_hist[i] = 0
+        end
+
+        for i=1,100 do
+            timer_mshist[i] = 0
+        end
+        timer_bounds = 0
+    end
+end
+
+
---
+++ suite-performance-lib.lua
@@ -0,0 +1,232 @@
+--
+-- PERFORMANCE RUNNERS --
+--
+
+local PERFRUN_HIST <const> = 1
+local PERFRUN_MSHIST <const> = 2
+local PERFRUN_OOB <const> = 3
+perfrun_stats = {}
+
+-- run one of these on each thread issuing performance tests. gathers the time
+-- history once per second and ships it for summarization.
+function perfrun_stats_out()
+    local t = {"===start==="}
+
+    for cmd, stats in pairs(perfrun_stats) do
+        table.insert(t, cmd)
+        local hist = stats[PERFRUN_HIST]
+        table.insert(t, table.concat(hist, ","))
+        local mshist = stats[PERFRUN_MSHIST]
+        table.insert(t, table.concat(mshist, ","))
+        table.insert(t, stats[PERFRUN_OOB])
+    end
+    if #t == 1 then
+        return -- nothing to send right now.
+    end
+
+    table.insert(t, "===end===")
+    mcs.out(t)
+
+    -- TODO: zero out the stats instead of wipe them to cut memory churn
+    -- slightly.
+    perfrun_stats = {}
+end
+
+-- run on separate stats thread. waits for data from test runner threads,
+-- summarizes and prints.
+function perfrun_stats_gather(a)
+    local tcount = 0
+    local lstats = {}
+    -- TODO: note thread count.
+    while true do
+        mcs.out_wait()
+
+        local rline = mcs.out_readline()
+        if rline ~= "===start===" then
+            error("expecting startline, got: " .. rline)
+        end
+
+        while true do
+            -- first line is the command bucket.
+            rline = mcs.out_readline()
+            if rline == "===end===" then
+                break
+            end
+
+            if lstats[rline] == nil then
+                lstats[rline] = perfrun_init_bucket_cmd()
+            end
+            local s = lstats[rline]
+
+            -- the next line is us histogram
+            -- the line after that is ms chart
+            -- final line os OOB count
+
+            rline = mcs.out_readline()
+            local i = 1
+            for num in string.gmatch(rline, '([^,]+)') do
+                s[PERFRUN_HIST][i] = s[PERFRUN_HIST][i] + tonumber(num)
+                i = i + 1
+            end
+
+            rline = mcs.out_readline()
+            i = 1
+            for num in string.gmatch(rline, '([^,]+)') do
+                s[PERFRUN_MSHIST][i] = s[PERFRUN_MSHIST][i] + tonumber(num)
+                i = i + 1
+            end
+
+            rline = mcs.out_readline()
+            s[PERFRUN_OOB] = s[PERFRUN_OOB] + tonumber(rline)
+        end
+
+        tcount = tcount + 1
+
+        if tcount == a.threads then
+            -- seen all threads, dump data.
+            tcount = 0
+            for cmd, s in pairs(lstats) do
+                local timer_hist = s[PERFRUN_HIST]
+                local timer_mshist = s[PERFRUN_MSHIST]
+                print("=== timer " .. cmd .. " ===")
+                print("1us", timer_hist[1])
+                print("10us", timer_hist[2])
+                print("100us", timer_hist[3])
+                for i=1,100 do
+                    if timer_mshist[i] > 0 then
+                        print(i .. "ms", timer_mshist[i])
+                    end
+                end
+                if s[PERFRUN_OOB] ~= 0 then
+                    print("100ms+:", s[PERFRUN_OOB])
+                end
+                print("=== end ===")
+            end
+
+            -- reset local stats cache.
+            lstats = {}
+        end
+    end
+end
+
+-- TODO: maybe use an add_custom to clear the bucket once, so runners can
+-- cache the global reference for a tiny speedup?
+function perfrun_init()
+    perfrun_stats = {}
+end
+
+function perfrun_init_bucket_cmd()
+    local stats = { {}, {}, 0}
+    -- seed this command bucket.
+    for i=1,3 do
+        table.insert(stats[PERFRUN_HIST], 0)
+    end
+
+    for i=1,100 do
+        table.insert(stats[PERFRUN_MSHIST], 0)
+    end
+    return stats
+end
+
+function perfrun_bucket(cmd, time)
+    local stats = perfrun_stats[cmd]
+    if stats == nil then
+        stats = perfrun_init_bucket_cmd()
+        perfrun_stats[cmd] = stats
+    end
+
+    local bucket = math.floor(math.log(time, 10) + 1)
+
+    if bucket > 5 then
+        stats[PERFRUN_OOB] = stats[PERFRUN_OOB] + 1
+    elseif bucket > 3 then
+        -- per ms granulairty
+        bucket = math.floor(time / 1000)
+        stats[PERFRUN_MSHIST][bucket] = stats[PERFRUN_MSHIST][bucket] + 1
+    else
+        -- histogram for sub-ms
+        stats[PERFRUN_HIST][bucket] = stats[PERFRUN_HIST][bucket] + 1
+    end
+end
+
+function perfrun_metaget(a)
+    local total_keys = a.limit
+    local res = mcs.res_new()
+    local req = mcs.mg_factory("perf", "v")
+    -- NOTE: this ends up resetting the global values a bunch of times, but we
+    -- need to ensure we do it once to clear any data from a previous run.
+    -- All of the inits run before any actual test code so this is fine.
+    perfrun_init()
+
+    return function()
+        local num = math.random(total_keys)
+        mcs.write_factory(req, num)
+        mcs.flush()
+
+        mcs.read(res)
+        local status, elapsed = mcs.match(req, res)
+        if not status then
+            print("mismatched response: " .. num .. " GOT: " .. mcs.resline(res))
+        end
+
+        perfrun_bucket("mg", elapsed)
+    end
+end
+
+function perfrun_metaget_pipe(a)
+    local total_keys = a.limit
+    local pipes = a.pipes
+    local reqfacs = {}
+    local results = {}
+    for i=1,pipes do
+        table.insert(results, mcs.res_new())
+        table.insert(reqfacs, mcs.mg_factory("perf", "v"))
+    end
+    perfrun_init()
+
+    return function()
+        for i=1,pipes do
+            local num = math.random(total_keys)
+            mcs.write_factory(reqfacs[i], num)
+        end
+        mcs.flush()
+
+        for i=1,pipes do
+            local res = results[i]
+            mcs.read(res)
+            local status, elapsed = mcs.match(reqfacs[i], res)
+            if not status then
+                print("mismatched response: " .. num .. " GOT: " .. mcs.resline(res))
+            end
+
+            perfrun_bucket("mg", elapsed)
+        end
+    end
+end
+
+function perfrun_metaset(a)
+    local total_keys = a.limit
+    local size = a.vsize
+    local res = mcs.res_new()
+    local req = mcs.ms_factory("perf", "")
+    perfrun_init()
+
+    return function()
+        local num = math.random(total_keys)
+        mcs.write_factory(req, num, vsize)
+        mcs.flush()
+
+        mcs.read(res)
+        local status, elapsed = mcs.match(req, res)
+        if not status then
+            print("mismatched response: " .. num .. " GOT: " .. mcs.resline(res))
+        end
+
+        perfrun_bucket("ms", elapsed)
+    end
+end
+
+--
+-- END PERFORMANCE RUNNERS
+--
+

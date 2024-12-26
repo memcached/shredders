aa390cc156bf356bec5d63005821085da2fd33c8
dormando <dormando@rydia.net>
2024-12-26 12:29:28 -0800

    get memstability actually running
    still sketching out what tests to actually run and I have some ideas...
    but lets start with making the basics work.

--- memstability/tests.lua
+++ memstability/tests.lua
@@ -2,6 +2,10 @@ local KByte <const> = 1000
 local MByte <const> = KByte * 1000
 local GByte <const> = MByte * 1000
 
+local basic_fill_size = 40 * GByte
+local small_item_size = 100
+local small_item_count = math.floor(basic_fill_size / small_item_size)
+
 local stats_arg = {
     stats = {
         "total_connections",
@@ -57,38 +61,41 @@ local function pfx(r)
 end
 
 local function go(r, time)
-    local prefix
-    if p then
-        prefix = p
-    else
-        prefix = pfx(r)
-    end
-
     -- do light time sampling to spot instability during test run
-    r:stats(timer_display)
-    r:stats(timer_conf, { prefix = prefix })
+    r:stats({ func = "timer_display", clients = 1, rate_limit = 1, init = true })
+    r:stats(timer_conf, { prefix = "timer/" })
     r:stats({ func = "stat_sample", clients = 1, rate_limit = 1 }, stats_arg)
     r:shred(time)
 
     -- grab stats snapshot before the server is stopped
-    r:work({ func = "perfrun_stats_out", rate_limit = 1, clients = 1, custom = true })
-    r:stats({ func = "perfrun_stats_gather", custom = true }, { threads = r:thread_count(), once = true })
+    -- TODO: restore once we're gathering stats from every func.
+    --r:work({ func = "perfrun_stats_out", rate_limit = 1, clients = 1, custom = true })
+    --r:stats({ func = "perfrun_stats_gather", custom = true }, { threads = r:thread_count(), once = true })
     r:stats({ func = "full_stats", custom = true }, {})
     r:stats({ func = "full_stats", custom = true }, { sub = "items" })
     r:stats({ func = "full_stats", custom = true }, { sub = "slabs" })
     r:shred()
+    plog("COMPLETE")
 end
 
 -- Per test: decide fill rate and calculate item count
+local test_get = {
+    n = "get",
+    f = function(r)
+        r:work({ func = "runner_basic", clients = 20, rate_limit = 0 },
+            { prefix = "test", vsize = small_item_size, total_keys = small_item_count })
+        go(r)
+    end
+}
 
 return {
     s = function()
-        nodestartdbg("mc-memory", "-c 250000 -m 40000", 1)
+        nodestartdbg("mc-memory", "-c 250000 -t 6 -m 40000", 1)
     end,
     e = function()
-        nodestop("mc-node1", 2)
+        nodestop("mc-memory", 2)
     end,
     t = {
-
+        test_get
     }
 }
